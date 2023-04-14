0aa20b580eb7a3da7b8c223150f71860aa6f381a
Ture-Dukale <86917357+Ture-Dukale@users.noreply.github.com>
2023-04-14 13:33:06 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -38,6 +38,5 @@ class Configure():
 		"""Display Configuration values."""
 		print("\nConfigurations:")
 		for a in dir(self):
-	    		if not a.startswith("__") and not callable(getattr(self, a)):
-				print("{:30} {}".format(a, getattr(self, a)))
+	    		if not a.startswith("__") and not callable(getattr(self, a)): print("{:30} {}".format(a, getattr(self, a)))
 		print("\n")
