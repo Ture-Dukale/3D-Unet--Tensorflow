e88c147db3543b7f84a554f1a34df663182e544d
Ture-Dukale <86917357+Ture-Dukale@users.noreply.github.com>
2023-04-15 20:19:40 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -8,10 +8,10 @@ class Configure():
 	# training
 	raw_data_dir = '/data/zhengyang/InfantBrain/RawData' # 'the directory where the raw data is stored'
 	data_dir = '/data/zhengyang/InfantBrain/tfrecords_full' # 'the directory where the input data is stored'
-	num_training_subs = 9 # 'the number of subjects used for training'
-	train_epochs = 10000 # 'the number of epochs to use for training'
-	epochs_per_eval = 50 # 'the number of training epochs to run between evaluations'
-	batch_size = 5 # 'the number of examples processed in each training batch'
+	num_training_subs = 4 # 'the number of subjects used for training'
+	train_epochs = 1000 # 'the number of epochs to use for training'
+	epochs_per_eval = 5 # 'the number of training epochs to run between evaluations'
+	batch_size = 1 # 'the number of examples processed in each training batch'
 	learning_rate = 1e-3 # 'learning rate'
 	weight_decay = 2e-6 # 'weight decay rate'
 	num_parallel_calls = 5 # 'The number of records that are processed in parallel \
@@ -21,16 +21,16 @@ class Configure():
 	model_dir = '/kaggle/working/model-10' # 'the directory where the model will be stored'
 
 	# validation / prediction
-	patch_size = 32 # 'spatial size of patches'
-	overlap_step = 8 # 'overlap step size when performing validation/prediction'
-	validation_id = 10 # '1-10 or -1, which subject is used for validation'
-	prediction_id = 11 # '1-23, which subject is used for prediction'
+	patch_size = 128 # 'spatial size of patches'
+	overlap_step = 32 # 'overlap step size when performing validation/prediction'
+	validation_id = 2 # '1-10 or -1, which subject is used for validation'
+	prediction_id = 6 # '1-23, which subject is used for prediction'
 	checkpoint_num = 1530 # 'which checkpoint is used for validation/prediction'
 	save_dir = '/kaggle/working/results' # 'the directory where the prediction is stored'
 
 	# network
 	network_depth = 3 # 'the network depth'
-	num_classes = 4 # 'the number of classes'
+	num_classes = 1+2 # 'the number of classes'
 	num_filters = 32 # 'number of filters for initial_conv'
 
 	
